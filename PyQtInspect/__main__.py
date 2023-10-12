d58c4aaddd278b0ef4ab28ca1cd040eff2d1d1e8
陈建彰 <chenjianzhang@corp.netease.com>
2023-10-12 10:06:48 +0800

    Add __main__.py

---
+++ PyQtInspect/__main__.py
@@ -0,0 +1,17 @@
+# -*- encoding:utf-8 -*-
+# ==============================================
+# Author: 陈建彰
+# Time: 2023/10/10 20:44
+# Description: 
+# ==============================================
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+if __name__ == '__main__':
+    import PyQtInspect.pqi
+
+    from PyQtInspect.pqi import SetupHolder
+
+    PyQtInspect.pqi.main()
