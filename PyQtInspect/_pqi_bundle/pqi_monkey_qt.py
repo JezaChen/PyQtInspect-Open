aa00add2235d3d1e1798255ad3ed84079ffdd3b2
陈建彰 <chenjianzhang@corp.netease.com>
2023-12-28 16:35:16 +0800

    QProcess支持hook(初步)

--- PyQtInspect/_pqi_bundle/pqi_monkey_qt.py
+++ PyQtInspect/_pqi_bundle/pqi_monkey_qt.py
@@ -5,6 +5,8 @@ import sys
 
 from PyQtInspect._pqi_bundle import pqi_log
 from PyQtInspect._pqi_bundle._pqi_monkey_qt_helpers import patch_QtWidgets
+from PyQtInspect._pqi_bundle.pqi_contants import IS_WINDOWS
+from PyQtInspect._pqi_bundle.pqi_monkey import str_to_args_windows, is_python, patch_args
 
 
 def set_trace_in_qt():
@@ -19,6 +21,46 @@ def set_trace_in_qt():
 IS_PY38 = sys.version_info[0] == 3 and sys.version_info[1] == 8
 
 _patched_qt = False
+
+_EMPTY = object()
+
+
+def _iterable(obj):
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+
+
+def _qt_split_command(command):
+    """ python reimplement of splitCommand in Qt """
+    args = []
+    tmp = ""
+    quoteCount = 0
+    inQuote = False
+
+    for i in range(len(command)):
+        if command[i] == '"':
+            quoteCount += 1
+            if quoteCount == 3:
+                quoteCount = 0
+                tmp += command[i]
+            continue
+        if quoteCount:
+            if quoteCount == 1:
+                inQuote = not inQuote
+            quoteCount = 0
+        if not inQuote and command[i].isspace():
+            if tmp:
+                args.append(tmp)
+                tmp = ""
+        else:
+            tmp += command[i]
+    if tmp:
+        args.append(tmp)
+
+    return args
 
 
 def patch_qt(qt_support_mode, is_attach=False):
@@ -185,6 +227,13 @@ def _internal_patch_qt(QtCore, qt_support_mode='auto'):
     _original_thread_init = QtCore.QThread.__init__
     _original_runnable_init = QtCore.QRunnable.__init__
     _original_QThread = QtCore.QThread
+    # === QProcess ===
+    # -- class --
+    _original_QProcess = QtCore.QProcess
+    # -- pure static methods --
+    _original_QProcess_execute = QtCore.QProcess.execute
+    # -- hybrid methods --
+    _original_QProcess_startDetached = QtCore.QProcess.startDetached
 
     class FuncWrapper:
         def __init__(self, original):
@@ -269,8 +318,198 @@ def _internal_patch_qt(QtCore, qt_support_mode='auto'):
             set_trace_in_qt()
             return self._pqi_original_run()
 
+    class QProcessWrapper(QtCore.QProcess):  # Wrapper for QProcess
+        # todo: for Qt6, QProcess has some new methods, need to be wrapped in the future
+        # Attention: May only support PyQt5!!!
+        # Attention: May only support Windows!!!
+        def __init__(self, *args):
+            _original_QProcess.__init__(self, *args)
+
+            # For patching
+            self._pqi_original_program = None
+            self._pqi_original_arguments = None
+
+            # Original methods
+            self._pqi_original_start = self.start
+            self._pqi_original_setProgram = self.setProgram
+            self._pqi_original_setArguments = self.setArguments
+            self._pqi_original_program_method = self.program
+            self._pqi_original_arguments_method = self.arguments
+            self._pqi_original_start_detached = self.startDetached
+            self._pqi_original_open = self.open
+
+            # Patched methods
+            # Cannot override methods directly
+            # Otherwise, the above _pqi_original_*** method would point to the overridden method.
+            # (The class is first defined and then __init__ is called.)
+            self.setArguments = self._pqi_new_setArguments
+            self.setProgram = self._pqi_new_setProgram
+            self.start = self._pqi_new_start
+            self.open = self._pqi_new_open
+            self.program = self._pqi_new_program_method
+            self.arguments = self._pqi_new_arguments_method
+
+        def _pqi_new_setProgram(self, program):
+            self._pqi_original_program = program
+            self._pqi_original_setProgram(program)
+
+        def _pqi_new_setArguments(self, arguments):
+            self._pqi_original_arguments = arguments
+            self._pqi_original_setArguments(arguments)
+
+        def _pqi_new_program_method(self):
+            if self._pqi_original_program is None:
+                return ''
+            return self._pqi_original_program
+
+        def _pqi_new_arguments_method(self):
+            if self._pqi_original_arguments is None:
+                return []
+            return self._pqi_original_arguments
+
+        def _pqi_patch_original_program_and_args(self):
+            if self._pqi_original_program is None or self._pqi_original_arguments is None:
+                # If the QProcess instance is reused, we need to restore the original program and arguments
+                # todo
+                # 如果复用, 则必须调用start、startDetached、open方法, 这种时候self._pqi_original_arguments都不会为None
+                # 且 self._pqi_original_program 和 self._pqi_original_arguments 可保证是原始的参数
+                # (都拦截到了, 且method3的调用前需要setProgram和setArguments方法)
+                #
+                # 没有复用，method3的调用有两个前提情况
+                # 前面调用过setProgram、setArguments方法: self._pqi_original_program和self._pqi_original_arguments都不为None
+                # 没有调用: 则self._pqi_original_program和self._pqi_original_arguments都为None, 这里会调用原始的program和arguments方法
+                self._pqi_original_program = self._pqi_original_program_method()
+                self._pqi_original_arguments = self._pqi_original_arguments_method()
+            arguments = [self._pqi_original_program] + self._pqi_original_arguments
+            patched_arguments = patch_args(arguments)
+            self._pqi_original_setProgram(patched_arguments[0])
+            self._pqi_original_setArguments(patched_arguments[1:])
+
+        def _pqi_new_start(self, __arg1=_EMPTY, __arg2=_EMPTY, __arg3=_EMPTY, *args, **kwargs):  # todo use custom objects
+            """
+            start(self, program: Optional[str], arguments: Iterable[Optional[str]], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+            start(self, command: Optional[str], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+            start(self, mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+            """
+            if args or kwargs:  # if there are other params, raise an error
+                raise RuntimeError('Not supported')
+
+            def _patch_method1():
+                # start(self, program: Optional[str], arguments: Iterable[Optional[str]], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+                nonlocal __arg1, __arg2
+
+                self._pqi_original_program, self._pqi_original_arguments = __arg1, __arg2
+                _arguments = [self._pqi_original_program] + self._pqi_original_arguments
+                _patched_arguments = patch_args(_arguments)
+                return _patched_arguments
+
+            def _patch_method2():
+                # start(self, command: Optional[str], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+                nonlocal __arg1
+                _arg_str = __arg1
+                _arguments = (str_to_args_windows if IS_WINDOWS else _qt_split_command)(__arg1)
+                self._pqi_original_program, *self._pqi_original_arguments = _arguments
+                if _arguments and is_python(_arguments[0]):
+                    _arg_str = ' '.join(patch_args(_arguments))
+                return _arg_str
+
+            def _patched_method3():
+                # start(self, mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+                self._pqi_patch_original_program_and_args()
+
+            if __arg1 is _EMPTY and __arg2 is _EMPTY and __arg3 is _EMPTY:  # no params
+                # start(self, mode = QIODevice.ReadWrite)
+                _patched_method3()
+                return self._pqi_original_start()
+            elif __arg2 is _EMPTY and __arg3 is _EMPTY:  # single param
+                if isinstance(__arg1, str):  # command
+                    # start(self, command: Optional[str], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+                    arg_str = _patch_method2()
+                    return self._pqi_original_start(arg_str)
+                else:  # mode?
+                    # start(self, mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag])
+                    _patched_method3()
+                    return self._pqi_original_start(__arg1)
+            elif __arg3 is _EMPTY:  # two params
+                if _iterable(__arg2):
+                    # start(self, program: Optional[str], arguments: Iterable[Optional[str]], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag] = QIODevice.ReadWrite)
+                    patched_arguments = _patch_method1()
+                    return self._pqi_original_start(patched_arguments[0], patched_arguments[1:])
+                else:
+                    # start(self, command: Optional[str], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag])
+                    arg_str = _patch_method2()
+                    return self._pqi_original_start(arg_str, __arg2)
+            else:  # three params
+                # start(self, program: Optional[str], arguments: Iterable[Optional[str]], mode: Union[QIODevice.OpenMode, QIODevice.OpenModeFlag])
+                patched_arguments = _patch_method1()
+                return self._pqi_original_start(patched_arguments[0], patched_arguments[1:], __arg3)
+
+        def _pqi_new_open(self, mode=_EMPTY, *args, **kwargs):
+            # we don't specify the default param mode as QIODevice.ReadWrite,
+            # because it may be different in different qt versions.
+            self._pqi_patch_original_program_and_args()
+            if mode is _EMPTY:
+                return self._pqi_original_open()
+            return self._pqi_original_open(mode, *args, **kwargs)
+
+        # Patch static methods
+        @staticmethod
+        def execute(__arg1, arguments=_EMPTY):
+            """
+            execute(program: str, arguments: Iterable[str]) -> int
+            execute(command: str) -> int # !!! it is a command!
+            """
+            if arguments is _EMPTY:  # __arg1 is a command
+                arguments = (str_to_args_windows if IS_WINDOWS else _qt_split_command)(__arg1)
+            else:  # __arg1 is a program
+                arguments = [__arg1] + arguments
+            patched_arguments = patch_args(arguments)
+            # call the original execute
+            return _original_QProcess_execute(patched_arguments[0], patched_arguments[1:])
+
+        # Patch hybrid methods (static or member)
+        def startDetached(__arg1=_EMPTY, __arg2=_EMPTY, __arg3=_EMPTY, *args, **kwargs):
+            """
+            startDetached(program: Optional[str], arguments: Iterable[Optional[str]], workingDirectory: Optional[str]) -> (bool, Optional[int])
+            startDetached(program: Optional[str], arguments: Iterable[Optional[str]])
+            startDetached(command: Optional[str]) #!!! it is a command!
+            startDetached(self) -> (bool, Optional[int])
+            """
+            if args or kwargs:  # if there are other params, raise an error
+                raise RuntimeError('Not supported')
+
+            if isinstance(__arg1, _original_QProcess):  # member method
+                # startDetached(self) -> (bool, Optional[int])
+                if __arg2 is not _EMPTY or __arg3 is not _EMPTY:
+                    raise TypeError('startDetached() takes 1 positional argument but 3 were given')
+                self = __arg1
+                self._pqi_patch_original_program_and_args()
+                return _original_QProcess_startDetached(self)
+            else:  # static method
+                if __arg1 is _EMPTY and __arg2 is _EMPTY and __arg3 is _EMPTY:  # no params
+                    # start(self, mode = QIODevice.ReadWrite)
+                    return _original_QProcess_startDetached()  # must raise an error
+                elif __arg2 is _EMPTY and __arg3 is _EMPTY:  # single param
+                    # startDetached(command: Optional[str])
+                    command = __arg1
+                    arguments = (str_to_args_windows if IS_WINDOWS else _qt_split_command)(command)
+                    patched_arguments = patch_args(arguments)
+                    patched_command = ' '.join(patched_arguments)
+                    return _original_QProcess_startDetached(patched_command)
+                elif __arg3 is _EMPTY:  # two params
+                    # startDetached(program: Optional[str], arguments: Iterable[Optional[str]])
+                    program, arguments = __arg1, __arg2
+                    patched_arguments = patch_args([program] + arguments)
+                    return _original_QProcess_startDetached(patched_arguments[0], patched_arguments[1:])
+                else:  # three params
+                    # startDetached(program: Optional[str], arguments: Iterable[Optional[str]], workingDirectory: Optional[str])
+                    program, arguments, workingDir = __arg1, __arg2, __arg3
+                    patched_arguments = patch_args([program] + arguments)
+                    return _original_QProcess_startDetached(patched_arguments[0], patched_arguments[1:], workingDir)
+
     QtCore.QThread = ThreadWrapper
     QtCore.QRunnable = RunnableWrapper
+    QtCore.QProcess = QProcessWrapper
 
 
 def _internal_patch_qt_widgets(QtWidgets, QtCore, QtGui, qt_support_mode='auto', is_attach=False):
