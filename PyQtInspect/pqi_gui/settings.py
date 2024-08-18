f8b2f8c0342d356a03927688f4bbdae852790f6f
Jeza <jeza@vip.qq.com>
2024-08-18 18:53:27 +0800

    Use new method for finding default PyCharm

--- PyQtInspect/pqi_gui/settings.py
+++ PyQtInspect/pqi_gui/settings.py
@@ -4,6 +4,8 @@
 # Time: 2023/9/7 16:11
 # Description: 
 # ==============================================
+import sys
+
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 settingsFile = "settings.ini"
@@ -31,7 +33,25 @@ def setCCTemplateSrcPath(path: str):
 
 
 def findDefaultPycharmPath():
-    import os
+    import os, subprocess
+
+    def findForWindows():
+        """ for Windows, we can use powershell command to find the path """
+        output = subprocess.run(
+            'powershell -Command "$(Get-Command pycharm).path"',
+            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            encoding='utf-8'
+        )
+        if output.stdout:
+            return output.stdout.strip()
+
+    # First, try to use terminal command to find the path
+    # TODO: add support for Linux and macOS
+    if sys.platform == "win32":
+        if defaultPath := findForWindows():
+            return defaultPath
+
+    # If the above method fails, we can try to find the path from the environment variables
     for path in os.environ["PATH"].split(";"):
         if "pycharm" in path.lower():
             return path + "\\pycharm64.exe"
