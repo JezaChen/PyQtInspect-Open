9b79295cb6f5763b11b3efa9853d90c9ff91958b
Jeza <jeza@vip.qq.com>
2024-09-17 20:18:09 +0800

    Update version -> 0.3.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description() -> str:
 
 setup(
     name="PyQtInspect",
-    version="0.3.5",
+    version="0.3.6",
     url="https://jeza-chen.com/PyqtInspect",
     author="Jianzhang Chen",
     author_email="jezachen@163.com",
